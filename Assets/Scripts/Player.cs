ee801560e25cba42645d75206540178b46f22fea
André N. Darcie <andrendarcie@gmail.com>
2017-03-19 16:42:13 -0300

    Added game HUD

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Player : MovingObject {
 
@@ -9,6 +10,7 @@ public class Player : MovingObject {
     public int pointsPerFood = 10;
     public int pointsPerSoda = 20;
     public float restartLevelDelay = 1f;
+    public Text foodText;
 
     private Animator animator;
     private int food;
@@ -19,6 +21,8 @@ public class Player : MovingObject {
         animator = GetComponent<Animator>();
 
         food = GameManager.instance.playerFoodPoints;
+
+        foodText.text = "Food: " + food;
 
         base.Start();
 	}
@@ -49,6 +53,7 @@ public class Player : MovingObject {
     protected override void AttemptMove <T> (int xDir, int yDir)
     {
         food--;
+        foodText.text = "Food: " + food;
 
         base.AttemptMove<T>(xDir, yDir);
 
@@ -69,11 +74,13 @@ public class Player : MovingObject {
         else if (other.tag == "Food")
         {
             food += pointsPerFood;
+            foodText.text = "+" + pointsPerFood + " Food: " + food;
             other.gameObject.SetActive(false);
         } 
         else if (other.tag == "Soda")
         {
             food += pointsPerSoda;
+            foodText.text = "+" + pointsPerSoda + " Food: " + food;
             other.gameObject.SetActive(false);
         }
     }
@@ -94,6 +101,7 @@ public class Player : MovingObject {
     {
         animator.SetTrigger("playerHit");
         food -= loss;
+        foodText.text = "-" + loss + " Food: " + food;
         CheckIfGameOver();
     }
 
