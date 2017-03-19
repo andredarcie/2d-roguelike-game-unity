1c2ad073523e51101ca2271853dd0496f4f469f9
André N. Darcie <andrendarcie@gmail.com>
2017-03-19 17:03:36 -0300

    Audio and Sound Manager

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -9,6 +9,8 @@ public class Enemy : MovingObject {
     private Animator animator;
     private Transform target;
     private bool skipMove;
+    public AudioClip enemyAttack1;
+    public AudioClip enemyAttack2;
 
 	protected override void Start() {
         GameManager.instance.AddEnemyToList(this);
@@ -50,8 +52,10 @@ public class Enemy : MovingObject {
     {
         Player hitPlayer = component as Player;
 
+        hitPlayer.LoseFood(playerDamage);
+
         animator.SetTrigger("enemyAttack");
 
-        hitPlayer.LoseFood(playerDamage);
+        SoundManager.instance.RandomizeSfx(enemyAttack1, enemyAttack2);
     }
 }
