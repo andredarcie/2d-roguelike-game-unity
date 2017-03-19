36a7f92ae8753f1f3f8fd32fcaa2129320ab6d77
André N. Darcie <andrendarcie@gmail.com>
2017-03-19 14:50:24 -0300

    Added the player script

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,6 +6,10 @@ public class GameManager : MonoBehaviour {
 
     public static GameManager instance = null;
     public BoardManager boardScript;
+    public int playerFoodPoints = 100;
+
+    [HideInInspector]
+    public bool playersTurn = true;
 
     private int level = 3;
 
@@ -25,6 +29,11 @@ public class GameManager : MonoBehaviour {
     void InitGame()
     {
         boardScript.SetupScene(level);
+    }
+
+    public void GameOver()
+    {
+        enabled = false;
     }
 	
 	// Update is called once per frame
